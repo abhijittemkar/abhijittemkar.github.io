678bd037f8722a74c43ead6176f966be498756d7
abhijittemkar <145930249+abhijittemkar@users.noreply.github.com>
2023-09-24 14:45:28 -0400

    Add files via upload

---
+++ A000174464 - Mental Health Awareness/assets/js/main.js
@@ -0,0 +1,121 @@
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$header = $('#header'),
+		$banner = $('#banner');
+
+	// Breakpoints.
+	breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large:  '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small:  '(max-width: 736px)',
+		xsmall: '(max-width: 480px)'
+	});
+
+	// Play initial animations on page load.
+	$window.on('load', function() {
+		window.setTimeout(function() {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
+
+	// Header.
+	if ($banner.length > 0 && $header.hasClass('alt')) {
+		// Simplify header handling for mobile.
+		function updateHeader() {
+			if ($window.width() <= 736) {
+				$header.removeClass('alt');
+			} else {
+				$header.addClass('alt');
+			}
+		}
+
+		// Update header on window resize.
+		$window.on('resize', updateHeader);
+
+		// Call it initially.
+		updateHeader();
+	}
+
+	// Menu.
+	var $menu = $('#menu');
+
+	$menu._locked = false;
+
+	$menu._lock = function() {
+		if ($menu._locked) return false;
+		$menu._locked = true;
+		window.setTimeout(function() {
+			$menu._locked = false;
+		}, 350);
+		return true;
+	};
+
+	$menu._show = function() {
+		if ($menu._lock()) $body.addClass('is-menu-visible');
+	};
+
+	$menu._hide = function() {
+		if ($menu._lock()) $body.removeClass('is-menu-visible');
+	};
+
+	$menu._toggle = function() {
+		if ($menu._lock()) $body.toggleClass('is-menu-visible');
+	};
+
+	$menu
+		.appendTo($body)
+		.on('click', function(event) {
+			event.stopPropagation();
+			// Simplify the menu hide logic for mobile.
+			if ($window.width() <= 736) {
+				$menu._toggle();
+			} else {
+				$menu._hide();
+			}
+		})
+		.find('.inner')
+			.on('click', '.close', function(event) {
+				event.preventDefault();
+				event.stopPropagation();
+				event.stopImmediatePropagation();
+				// Simplify the menu hide logic for mobile.
+				if ($window.width() <= 736) {
+					$menu._toggle();
+				} else {
+					$menu._hide();
+				}
+			})
+			.on('click', function(event) {
+				event.stopPropagation();
+			})
+			.on('click', 'a', function(event) {
+				var href = $(this).attr('href');
+				event.preventDefault();
+				event.stopPropagation();
+				// Simplify the menu hide logic for mobile.
+				if ($window.width() <= 736) {
+					$menu._toggle();
+				} else {
+					$menu._hide();
+				}
+				// Redirect.
+				window.setTimeout(function() {
+					window.location.href = href;
+				}, 350);
+			});
+
+	$body
+		.on('click', 'a[href="#menu"]', function(event) {
+			event.stopPropagation();
+			event.preventDefault();
+			$menu._toggle();
+		})
+		.on('keydown', function(event) {
+			// Hide on escape.
+			if (event.keyCode == 27) $menu._hide();
+		});
+
+})(jQuery);
